082ba812b04101133d1f6cace5680392e691663f
Akshaya Viswanathan <akshayalakshmibv@gmail.com>
2023-02-21 20:28:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var randomNumber1 = Math.floor(((Math.random() * 6) + 1));
-document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
+var path1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src",path1);
 
 var randomNumber2 = Math.floor(((Math.random() * 6) + 1));
-document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+var path2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src",path2);
 
 if (randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
