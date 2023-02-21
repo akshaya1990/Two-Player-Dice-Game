36ab5448e525587e66c13c2fab0230340b12cafe
Akshaya Viswanathan <akshayalakshmibv@gmail.com>
2023-02-21 20:33:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ var path2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src",path2);
 
 if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!!!";
 }
 else if (randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "Player 2 Wins🚩!";
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩!!!";
 }
 else{
-    document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML = "Draw!!!";
 }
